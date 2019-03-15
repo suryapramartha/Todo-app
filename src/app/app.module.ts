85906ebb962071b157d96b0ae71abc19d50c5f8b
suryapramartha <uya.hardline@gmail.com>
2019-03-15 16:35:39 +0800

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: '', component : HomeComponent},
+      {path: 'login', component : LoginComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
