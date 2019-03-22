b20e0e4fc2fe3c0a951b8e5413012e5adc9a7a4e
suryapramartha <uya.hardline@gmail.com>
2019-03-22 14:37:51 +0800

    fixing issue on package.json

--- src/app/server.js
+++ src/app/server.js
@@ -1,16 +1,17 @@
-//Install express server
+//server.js
+
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/todo-app'));
+const path = require('path');
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/todo-app/index.html'));
+app.use(express.static('./dist/todo-app'));
+
+app.get('/*', function(req, res) {
+
+  res.sendFile(path.join('./dist/todo-app/index.html'));
+
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
